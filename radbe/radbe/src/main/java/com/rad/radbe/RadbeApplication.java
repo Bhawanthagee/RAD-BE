3e5fb4165749b1f6608fc84cdc3241eed6f3ee09
bawantha Geeganage <47896441+Bhawanthagee@users.noreply.github.com>
2025-05-16 20:14:11 +0530

    Initial commit

---
+++ radbe/radbe/src/main/java/com/rad/radbe/RadbeApplication.java
@@ -0,0 +1,13 @@
+package com.rad.radbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RadbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RadbeApplication.class, args);
+	}
+
+}
---
+++ radbe/radbe/src/test/java/com/rad/radbe/RadbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rad.radbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RadbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
